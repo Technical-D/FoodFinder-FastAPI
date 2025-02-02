06f8ecc6508adc866113ede042caaf6352c4c7e6
Dheeraj Gupta <dheerajgupta1902@gmail.com>
2025-02-02 12:31:03 +0530

    fix:param-updated

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ app = FastAPI()
 FOOD_SEARCH_URL = "https://platform.fatsecret.com/rest/server.api"
 
 @app.get("/search")
-def search_food(query:str):
+def search_food(query:str, page_number:int=0, max_results:int=10):
     token = get_access_token()
     params = {
-        "method": "foods.search.v3",
+        "method": "foods.search",
         "format": "json",
         "search_expression": query,
+        "page_number": page_number,
+        "max_results": max_results
     }
 
     headers = {"Authorization": f"Bearer {token}"}
