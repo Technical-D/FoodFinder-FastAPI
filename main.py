c5ec0c7113248a9f08816696fcfa8bfdf425e0ee
Dheeraj Gupta <dheerajgupta1902@gmail.com>
2025-02-14 12:00:00 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -25,4 +25,3 @@ def search_food(query:str, page_number:int=0, max_results:int=10):
         return response.json()
     else:
         raise HTTPException(status_code=response.status_code, detail="Failed to fetch data")
-    
